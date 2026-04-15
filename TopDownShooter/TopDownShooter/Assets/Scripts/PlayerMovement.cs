53da4e4edd4e66bdf8d5b91c50df15db7849e01e
ToastyKitty48 <cooper.lyon@charter.net>
2026-04-15 13:15:44 -0700

    Cleaned up PlayerMovement and fixed some movement bugs

--- TopDownShooter/TopDownShooter/Assets/Scripts/PlayerMovement.cs
+++ TopDownShooter/TopDownShooter/Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,11 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 4.0f;
     public float jumpForce = 2.0f;
     [SerializeField] float extraFallMutiplyer = 2;
-    [SerializeField] Vector2 groundCheckBoxCastSize = new Vector2(0.9f, 0.1f);
-    [SerializeField] LayerMask groundLayer;
+    [SerializeField] ContactFilter2D contactFilter;
+    bool isGrounded => rb.IsTouching(contactFilter);
     Rigidbody2D rb;
     float defaultGravity;
-    bool grounded;
+    bool jumpPressed = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -19,29 +19,41 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
-        ExtraGravOnFall();
-        if (Input.GetButtonDown("Jump") && IsGrounded() && !Input.GetButton("Fire1"))
+        //gets imput status for next fixed update call
+        if (Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(new Vector2(0, jumpForce * 50));
+            jumpPressed = true;
         }
     }
     void FixedUpdate()
     {
-        if (!Input.GetButton("Fire1") || !IsGrounded())
-        {
-            RunPlayerMovement();
-        }
+        RunPlayerMovement();
+        AttemptJump();
+        ExtraGravOnFall();
     }
     void RunPlayerMovement()
     {
-        float x = Input.GetAxis("Horizontal");
-        Vector2 vel = rb.velocity;
-        vel.x = x * moveSpeed;
-        rb.velocity = vel;
+        //allows player to move if they are not actively shooting
+        if (!Input.GetButton("Fire1"))
+        {
+            float x = Input.GetAxis("Horizontal");
+            Vector2 vel = rb.velocity;
+            vel.x = x * moveSpeed;
+            rb.velocity = vel;
+        }
+    }
+    void AttemptJump()
+    {
+        if (jumpPressed && isGrounded && !Input.GetButton("Fire1"))
+        {
+            rb.AddForce(new Vector2(0, jumpForce * 50));
+        }
+        //resets jumpPressed for next check
+        jumpPressed = false;
     }
     void ExtraGravOnFall()
     {
-        //If player is falling mutiply applied gravity by extraFallMutiplyer
+        //if player is falling mutiply applied gravity by extraFallMutiplyer
         if (rb.velocity.y < 0)
         {
             rb.gravityScale = defaultGravity * extraFallMutiplyer;
@@ -50,18 +62,5 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.gravityScale = defaultGravity;
         }
-    }
-    public bool IsGrounded()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 0.55f * transform.lossyScale.y);
-        //RaycastHit2D hit = Physics2D.BoxCast(transform.position, groundCheckBoxCastSize, 0, new Vector2 (0, 0.55f * transform.lossyScale.y), groundLayer);
-        grounded = hit.collider != null && hit.collider.gameObject.name != gameObject.name;
-
-        return grounded;
-    }
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawRay(transform.position, new Vector2(0, -0.55f * transform.lossyScale.y));
-        Gizmos.DrawWireCube(transform.position, groundCheckBoxCastSize);
     }
 }
