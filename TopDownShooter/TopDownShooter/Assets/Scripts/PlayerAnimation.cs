0e1d0260fc124945703932f0e449cd8b56ca6d0a
Devin <mcmanusdevin2008@gmail.com>
2026-04-27 13:20:22 -0700

    Working on player animations

--- TopDownShooter/TopDownShooter/Assets/Scripts/PlayerAnimation.cs
+++ TopDownShooter/TopDownShooter/Assets/Scripts/PlayerAnimation.cs
@@ -7,11 +7,6 @@ public class PlayerAnimation : MonoBehaviour
 {
 
     [SerializeField] private Animator _animator;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -19,6 +14,15 @@ public class PlayerAnimation : MonoBehaviour
         if (Input.GetKey(key: KeyCode.D))
         {
             _animator.SetBool("isRunningRight", true);
+            _animator.SetBool("isRunningLeft", false);
+            _animator.SetBool("isShootingTopLeft", false);
+            _animator.SetBool("isShootingLeft", false);
+            _animator.SetBool("isShootingBotttomLeft", false);
+            _animator.SetBool("isShootingDown", false);
+            _animator.SetBool("isShootingBottomRight", false);
+            _animator.SetBool("isShootingRight", false);
+            _animator.SetBool("isShootingTopRight", false);
+            _animator.SetBool("isShootingUp", false);
         }
         else
         {
@@ -30,20 +34,156 @@ public class PlayerAnimation : MonoBehaviour
         if (Input.GetKey(key: KeyCode.A))
         {
             _animator.SetBool("isRunningLeft", true);
-        } else
-        {
+            _animator.SetBool("isRunningRight", false);
+            _animator.SetBool("isShootingTopLeft", false);
+            _animator.SetBool("isShootingLeft", false);
+            _animator.SetBool("isShootingBotttomLeft", false);
+            _animator.SetBool("isShootingDown", false);
+            _animator.SetBool("isShootingBottomRight", false);
+            _animator.SetBool("isShootingRight", false);
+            _animator.SetBool("isShootingTopRight", false);
+            _animator.SetBool("isShootingUp", false);
+        }
+        else {
             _animator.SetBool("isRunningLeft", false);
         }
 
         if (Input.GetKey(key: KeyCode.W) &&
             Input.GetKey(key: KeyCode.A) &&
-            Input.GetKey(key: KeyCode.Mouse1))
+            Input.GetKey(key: KeyCode.Mouse0))
                 {
             _animator.SetBool("isShootingTopLeft", true);
-        } else
-        {
+            _animator.SetBool("isRunningRight", false);
+            _animator.SetBool("isRunningLeft", false);
+            _animator.SetBool("isShootingLeft", false);
+            _animator.SetBool("isShootingBotttomLeft", false);
+            _animator.SetBool("isShootingDown", false);
+            _animator.SetBool("isShootingBottomRight", false);
+            _animator.SetBool("isShootingRight", false);
+            _animator.SetBool("isShootingTopRight", false);
+            _animator.SetBool("isShootingUp", false);
+        } else {
             _animator.SetBool("isShootingTopLeft", false);
         }
+
+        if (Input.GetKey(key: KeyCode.A) &&
+            Input.GetKey(key: KeyCode.Mouse0))
+        {
+            _animator.SetBool("isShootingLeft", true);
+            _animator.SetBool("isRunningRight", false);
+            _animator.SetBool("isRunningLeft", false);
+            _animator.SetBool("isShootingTopLeft", false);
+            _animator.SetBool("isShootingBotttomLeft", false);
+            _animator.SetBool("isShootingDown", false);
+            _animator.SetBool("isShootingBottomRight", false);
+            _animator.SetBool("isShootingRight", false);
+            _animator.SetBool("isShootingTopRight", false);
+            _animator.SetBool("isShootingUp", false);
+        } else {
+        _animator.SetBool("isShootingLeft", false);
+                }
+        if (Input.GetKey(key: KeyCode.A) &&
+            Input.GetKey(key:KeyCode.S) &&
+            Input.GetKey(key:KeyCode.Mouse0))
+        {
+            _animator.SetBool("isShootingBottomLeft", true);
+            _animator.SetBool("isRunningRight", false);
+            _animator.SetBool("isRunningLeft", false);
+            _animator.SetBool("isShootingTopLeft", false);
+            _animator.SetBool("isShootingLeft", false);
+            _animator.SetBool("isShootingDown", false);
+            _animator.SetBool("isShootingBottomRight", false);
+            _animator.SetBool("isShootingRight", false);
+            _animator.SetBool("isShootingTopRight", false);
+            _animator.SetBool("isShootingUp", false);
+        } else {
+            _animator.SetBool("isShootingBotttomLeft", false);
+        }
+        if (Input.GetKey(key:KeyCode.S) &&
+            Input.GetKey(key:KeyCode.Mouse0))
+        {
+            _animator.SetBool("isShootingDown", true);
+            _animator.SetBool("isRunningRight", false);
+            _animator.SetBool("isRunningLeft", false);
+            _animator.SetBool("isShootingTopLeft", false);
+            _animator.SetBool("isShootingLeft", false);
+            _animator.SetBool("isShootingBotttomLeft", false);
+            _animator.SetBool("isShootingBottomRight", false);
+            _animator.SetBool("isShootingRight", false);
+            _animator.SetBool("isShootingTopRight", false);
+            _animator.SetBool("isShootingUp", false);
+        } else {
+            _animator.SetBool("isShootingDown", false);
+        }
+        if (Input.GetKey(key: KeyCode.S) &&
+            Input.GetKey(key: KeyCode.D) &&
+            Input.GetKey(key: KeyCode.Mouse0))
+        {
+            _animator.SetBool("isShootingBottomRight", true);
+            _animator.SetBool("isRunningRight", false);
+            _animator.SetBool("isRunningLeft", false);
+            _animator.SetBool("isShootingTopLeft", false);
+            _animator.SetBool("isShootingLeft", false);
+            _animator.SetBool("isShootingBotttomLeft", false);
+            _animator.SetBool("isShootingDown", false);
+            _animator.SetBool("isShootingRight", false);
+            _animator.SetBool("isShootingTopRight", false);
+            _animator.SetBool("isShootingUp", false);
+        } else {
+            _animator.SetBool("isShootingBottomRight", false);
+        }
+        if (Input.GetKey(key: KeyCode.D) &&
+            Input.GetKey(key: KeyCode.Mouse0))
+        {
+            _animator.SetBool("isShootingRight", true);
+            _animator.SetBool("isRunningRight", false);
+            _animator.SetBool("isRunningLeft", false);
+            _animator.SetBool("isShootingTopLeft", false);
+            _animator.SetBool("isShootingLeft", false);
+            _animator.SetBool("isShootingBotttomLeft", false);
+            _animator.SetBool("isShootingDown", false);
+            _animator.SetBool("isShootingBottomRight", false);
+            _animator.SetBool("isShootingTopRight", false);
+            _animator.SetBool("isShootingUp", false);
+        } else {
+            _animator.SetBool("isShootingRight", false);
+        }
+        if (Input.GetKey(key: KeyCode.W) &&
+            Input.GetKey(key: KeyCode.D) &&
+            Input.GetKey(key: KeyCode.Mouse0))
+        {
+            _animator.SetBool("isShootingTopRight", true);
+            _animator.SetBool("isRunningRight", false);
+            _animator.SetBool("isRunningLeft", false);
+            _animator.SetBool("isShootingTopLeft", false);
+            _animator.SetBool("isShootingLeft", false);
+            _animator.SetBool("isShootingBotttomLeft", false);
+            _animator.SetBool("isShootingDown", false);
+            _animator.SetBool("isShootingBottomRight", false);
+            _animator.SetBool("isShootingRight", false);
+            _animator.SetBool("isShootingUp", false);
+        } else {
+            _animator.SetBool("isShootingTopRight", false);
+        }
+        if (Input.GetKey(key: KeyCode.W) &&
+            Input.GetKey(key: KeyCode.Mouse0))
+        {
+            _animator.SetBool("isShootingUp", true);
+            _animator.SetBool("isRunningRight", false);
+            _animator.SetBool("isRunningLeft", false);
+            _animator.SetBool("isShootingTopLeft", false);
+            _animator.SetBool("isShootingLeft", false);
+            _animator.SetBool("isShootingBotttomLeft", false);
+            _animator.SetBool("isShootingDown", false);
+            _animator.SetBool("isShootingBottomRight", false);
+            _animator.SetBool("isShootingRight", false);
+            _animator.SetBool("isShootingTopRight", false);
+        }
+        else
+        {
+            _animator.SetBool("isShootingUp", false);
+        }
+
     }
 
 }
