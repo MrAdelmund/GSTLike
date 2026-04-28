2eadc34cde706b6d2846d94f4e99d27c2f5055d6
ToastyKitty48 <cooper.lyon@charter.net>
2026-04-28 13:29:41 -0700

    Working on bullet behavior

--- TopDownShooter/TopDownShooter/Assets/Scripts/DestroyOnHit.cs
+++ TopDownShooter/TopDownShooter/Assets/Scripts/DestroyOnHit.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class DestroyOnHit : MonoBehaviour
 {
+    [SerializeField] GameObject HitExplodePrefab;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(gameObject);
+        if (HitExplodePrefab != null)
+            Instantiate(HitExplodePrefab, transform.position, transform.rotation);
     }
 }
