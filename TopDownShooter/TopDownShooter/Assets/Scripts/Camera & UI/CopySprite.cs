c69788aef5f865fa1829ec9660ba3fa12eec9ec9
ToastyKitty48 <cooper.lyon@charter.net>
2026-05-01 13:22:05 -0700

    Moving scripts

--- TopDownShooter/TopDownShooter/Assets/Scripts/Camera & UI/CopySprite.cs
+++ TopDownShooter/TopDownShooter/Assets/Scripts/Camera & UI/CopySprite.cs
@@ -5,13 +5,6 @@ using UnityEngine.UI;
 public class CopySprite : MonoBehaviour
 {
     public Image image;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         GetComponent<Image>().sprite = image.sprite;
