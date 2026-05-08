c0e40dec0191ac0ddba7f4ada56c6b631d9b4324
ToastyKitty48 <cooper.lyon@charter.net>
2026-05-08 13:59:23 -0700

    Working on getting ui working

---
+++ TopDownShooter/TopDownShooter/Assets/Scripts/Camera & UI/PauseMenuControler.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseMenuControler : MonoBehaviour
+{
+    [SerializeField] Animator animatorForAnimation;
+    static bool pauseMenuCanOpen = true;
+    bool menuButtonPressed = false;
+    Canvas canvas;
+    bool toggeledOn = false;
+
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+        animatorForAnimation.enabled = false;
+    }
+    public void TogglePauseMenu(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            toggeledOn = !toggeledOn;
+        if (toggeledOn)
+            Pause();
+        else
+            Resume();
+    }
+    void Pause()
+    {
+        canvas.enabled = true;
+        Time.timeScale = 0;
+        animatorForAnimation.enabled = true;
+
+    }
+    public void Resume()
+    {
+        canvas.enabled = false;
+        Time.timeScale = 1;
+        animatorForAnimation.enabled = false;
+
+    }
+    public void Restart()
+    {
+
+    }
+    public void ReturnToMenu()
+    {
+
+    }
+    public static void DisablePauseMenuOpening()
+    {
+        pauseMenuCanOpen = false;
+    }
+    public static void EnablePauseMenuOpening()
+    {
+        pauseMenuCanOpen = false;
+    }
+}
