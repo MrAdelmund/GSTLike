9066d1149dbca04df7e7a80d365d6972cba2c13d
ToastyKitty48 <cooper.lyon@charter.net>
2026-05-08 12:29:56 -0700

    Finishing Basic Enemy Spawning

--- TopDownShooter/TopDownShooter/Assets/Scripts/Enemy/EnemySpawner.cs
+++ TopDownShooter/TopDownShooter/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -11,16 +11,15 @@ public class EnemySpawner : MonoBehaviour
     [Header("References")]
     [SerializeField] GameObject meleeEnemyPrefab;
     [SerializeField] GameObject rangedEnemyPrefab;
-    static int rangedEnemySpawnBacklog = 5;
-    static int meleeEnemySpawnBacklog = 5;
+    static int rangedEnemySpawnBacklog = 0;
+    static int meleeEnemySpawnBacklog = 0;
+    //spawnEnemies bool is for if the spawner is allowed to spawn enemies or not, and is controled by EnemySpawnerBox.
+    //It can be used to disable spawning even if the spawn queue has enemies left.
     static bool spawnEnemies = true;
-    bool doSpawnEnemiesCheck = true;
+    //doSpawnEnemiesCheck bool stops spawnTimer from being ticked up, this will stop enemies from spawning. 
+    //It gets triggered when no enemies are left in the spawn queue.
+    static bool doSpawnEnemiesCheck = true;
     float spawnTimer = 0;
-    Rigidbody2D playerRb;
-    void Start()
-    {
-        playerRb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
-    }
     private void Update()
     {
         if (doSpawnEnemiesCheck)
@@ -33,36 +32,38 @@ public class EnemySpawner : MonoBehaviour
     }
     void SpawnEnemies()
     {
+        //reset timer
         spawnTimer = 0;
-        int randomEnemyToSpawn = Random.Range(0, meleeEnemySpawnBacklog + rangedEnemySpawnBacklog);
-        if (randomEnemyToSpawn < rangedEnemySpawnBacklog)
-        {
-            Instantiate(rangedEnemyPrefab, GetSpawnLocation(), Quaternion.Euler(Vector3.zero));
-            rangedEnemySpawnBacklog--;
-        }else
-        {
-            Instantiate(meleeEnemyPrefab, GetSpawnLocation(), Quaternion.Euler(Vector3.zero));
-            meleeEnemySpawnBacklog--;
-        }
+        //stops enemy spawning if queue is empty
         if (meleeEnemySpawnBacklog <= 0 && rangedEnemySpawnBacklog <= 0)
             doSpawnEnemiesCheck = false;
+        else
+        {
+            //gets a random enemy to spawn, gives more weight to the larger queue.
+            int randomEnemyToSpawn = Random.Range(0, meleeEnemySpawnBacklog + rangedEnemySpawnBacklog);
+            if (randomEnemyToSpawn < rangedEnemySpawnBacklog)
+            {
+                Instantiate(rangedEnemyPrefab, GetSpawnLocation(), Quaternion.Euler(Vector3.zero));
+                rangedEnemySpawnBacklog--;
+            }else
+            {
+                Instantiate(meleeEnemyPrefab, GetSpawnLocation(), Quaternion.Euler(Vector3.zero));
+                meleeEnemySpawnBacklog--;
+            }
+        }
     }
     Vector2 GetSpawnLocation()
     {
         Vector2 spawnPos = transform.position;
         bool spawnSide = (Random.Range(0, 2) == 1) ? true : false; //generates a random bool value for the direction the enemy should be spawned in
         spawnPos = new Vector2(spawnPos.x + (horizontalSpawnDist * ((spawnSide) ? -1 : 1)), spawnPos.y + verticalSpawnDist);
-        if (spawnSide && playerRb.velocity.x < 0)
-            spawnPos = new Vector2(spawnPos.x * 1.5f, spawnPos.y);
-        else if (!spawnSide && playerRb.velocity.x > 0)
-            spawnPos = new Vector2(spawnPos.x * 1.5f, spawnPos.y);
-
         return spawnPos;
     }
     public static void AddToEnemySpawnQueue(int amountOfRangedEnemies, int amountOfMeleeEnemies)
     {
         rangedEnemySpawnBacklog += amountOfRangedEnemies;
         meleeEnemySpawnBacklog += amountOfMeleeEnemies;
+        doSpawnEnemiesCheck = true;
     }
     public static void PauseEnemySpawning()
     {
@@ -76,5 +77,10 @@ public class EnemySpawner : MonoBehaviour
     {
         rangedEnemySpawnBacklog = 0;
         meleeEnemySpawnBacklog = 0;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(new Vector2(horizontalSpawnDist, verticalSpawnDist), Vector2.one);
+        Gizmos.DrawWireCube(new Vector2(-horizontalSpawnDist, verticalSpawnDist), Vector2.one);
     }
 }
--- TopDownShooter/TopDownShooter/Assets/Scripts/Enemy/EnemySpawnerTriggerBox.cs
+++ TopDownShooter/TopDownShooter/Assets/Scripts/Enemy/EnemySpawnerTriggerBox.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class EnemySpawnerTriggerBox : MonoBehaviour
 {
+    [Header("Enemy Spawning")]
+    [SerializeField] bool addToEnemySpawnQueue = true;
     [SerializeField] int amountOfRangedEnemiesToAdd = 5;
     [SerializeField] int amountOfMeleeEnemiesToAdd = 5;
+    [Header("Additon Functions")]
+    [SerializeField] bool pauseEnemySpawning = false;
+    [SerializeField] bool resumeEnemySpawning = false;
+    [SerializeField] bool clearEnemySpawnQueue = false;
+    bool ran = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
-            EnemySpawner.AddToEnemySpawnQueue(amountOfRangedEnemiesToAdd, amountOfMeleeEnemiesToAdd);
+        if (!ran && collision.gameObject.tag == "Player")
+        {
+            ran = true;
+            Debug.Log("im runningggggggggg :3");
+            if (addToEnemySpawnQueue)
+                EnemySpawner.AddToEnemySpawnQueue(amountOfRangedEnemiesToAdd, amountOfMeleeEnemiesToAdd);
+            if (pauseEnemySpawning)
+                EnemySpawner.PauseEnemySpawning();
+            if (resumeEnemySpawning)
+                EnemySpawner.ResumeEnemySpawning();
+            if (clearEnemySpawnQueue)
+                EnemySpawner.ClearEnemySpawnQueue();
+        }
     }
 }
